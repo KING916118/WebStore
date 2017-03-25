4c9d9c41bc42a5776ed8443824e01acdf7f03112
caolei <clei118@foxmail.com>
2017-03-25 14:58:56 +0800

    test update

---
+++ 2.js
@@ -0,0 +1,14 @@
+/**
+ * Created by caolei on 2017/3/25.
+ */
+/**
+ * Created by caolei on 2017/3/25.
+ */
+module.exports={
+    login:function(req,res){
+        res.write('用户登录');
+    },
+    logout:function(req,res){
+        res.write('用户注销');
+    }
+}
---
+++ 3.js
@@ -0,0 +1,4 @@
+/**
+ * Created by caolei on 2017/3/25.
+ */
+console.log("test update");
